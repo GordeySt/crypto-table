3de77e9de8c8e6b9f34d984ed16748987bfe9f91
gordeyst <gordeystahovets@gmail.com>
2020-07-17 23:13:33 +0300

    Modified App.tsx/index.tsx, created styles.js

---
+++ src/styles.js
@@ -0,0 +1,19 @@
+import { createUseStyles } from "react-jss";
+
+const rootStyle = {
+  width: 400,
+  border: "1px solid grey",
+  margin: "0px auto",
+  padding: 15
+};
+
+const fontStyle = {
+  fontWeight: "bold",
+  fontStyle: "italic"
+};
+
+const useStyles = createUseStyles({
+  
+});
+
+export default useStyles;
